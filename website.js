ef4817ccf45718675e4b49a591795d10a809fa8d
lagunita94 <barick.abhijeet@gmail.com>
2018-11-06 00:00:42 +0530

    commit

---
+++ website.js
@@ -0,0 +1,49 @@
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+var img4 = document.getElementById("img4");
+
+img1.addEventListener("mouseenter", function(){
+    this.style.padding = "10px 10px"
+    this.style.height = "400px"
+    this.style.transitionDuration = "0.5s"
+});
+img1.addEventListener("mouseleave", function(){
+    this.style.padding = ""
+    this.style.height = ""
+    this.style.transitionDuration = "0.5s"
+    
+});
+img2.addEventListener("mouseenter", function(){
+    this.style.padding = "10px 10px"
+    this.style.height = "400px"
+    this.style.transitionDuration = "0.5s"
+});
+img2.addEventListener("mouseleave", function(){
+    this.style.padding = ""
+    this.style.height = ""
+    this.style.transitionDuration = "0.5s"
+    
+});
+img3.addEventListener("mouseenter", function(){
+    this.style.padding = "10px 10px"
+    this.style.height = "400px"
+    this.style.transitionDuration = "0.5s"
+});
+img3.addEventListener("mouseleave", function(){
+    this.style.padding = ""
+    this.style.height = ""
+    this.style.transitionDuration = "0.5s"
+    
+});
+img4.addEventListener("mouseenter", function(){
+    this.style.padding = "10px 10px"
+    this.style.height = "400px"
+    this.style.transitionDuration = "0.5s"
+});
+img4.addEventListener("mouseleave", function(){
+    this.style.padding = ""
+    this.style.height = ""
+    this.style.transitionDuration = "0.5s"
+    
+});
